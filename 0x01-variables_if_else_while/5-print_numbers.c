30c18cdcbadf60d3b91055d9c3095781063d6626
yvansamen237 <samenyvan@gmail.com>
2023-02-20 16:19:19 -0800

    msg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all the digits of base 10
+ * Return: 0 always
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
